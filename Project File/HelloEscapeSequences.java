f9d508381467b7c055f41e806f616b30580ddcce
Akhil Pandey <akhilpandey494@gmail.com>
2025-06-26 14:13:18 +0000

    Hii

---
+++ Project File/HelloEscapeSequences.java
@@ -0,0 +1,11 @@
+public class HelloEscapeSequences {
+public static void main(String[] args) {
+System.out.println("Hello, Java World from Eclipse!");
+System.out.println("Newline character:\\n creates a new line\nThis is a new line.");
+System.out.println("Tab character:\\t adds a tab\tSee the tab space.");
+System.out.println("Backslash character:\\\\ displays a backslash \\");
+System.out.println("Double quote character:\\\" displays a double quote \"");
+System.out.println("Single quote character:\\' displays a single quote \'");
+System.out.println("Carriage return character:\\r will return to start of the line\rOverwrite");
+}
+}
---
+++ UNIT-4/hashmap.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class hashmap {
+    public static void main(String[] args){
+        Map<String,Integer> hs=new HashMap<>();
+        hs.put("Hii", 1);
+        hs.put("RCB", 180);
+        hs.put("a", 65);
+        hs.put("b", 66);
+        System.out.println(hs);
+    }
+}
